d9206dfc6303824db861e91f90199f68516f3479
Avery <44121063+Avery007@users.noreply.github.com>
2021-02-08 13:24:53 -0500

    added movement

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+    public Vector3 rotation = new Vector3(0, 2.0f, 0);
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,34 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position= this.transform.position+ new Vector3(1.0f,0,0);
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position = this.transform.position - new Vector3(1.0f, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position = this.transform.position + new Vector3(0, 0, 1.0f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position = this.transform.position - new Vector3(0, 0, 1.0f);
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            this.GetComponent<Transform>().Rotate(rotation);
+        }
+
+
+
+
     }
 }
